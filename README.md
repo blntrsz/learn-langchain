68736694cff9dc55fa6a780a1c12caecbfe96a8a
Balint Orosz <orosz.balint.97@gmail.com>
2023-09-07 20:16:27 +0200

    create first llm

---
+++ README.md
@@ -0,0 +1,19 @@
+# Learning LangChain
+
+1. Create  `.env` with OPENAI_API_KEY api key
+
+2. To activate pipenv run:
+```
+pipenv shell
+```
+
+3. Install dependencies
+```
+pipenv install
+```
+
+4. Run program:
+```
+python <program-name>.py
+```
+
---
+++ sentence_shortener.py
@@ -0,0 +1,20 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+information = input("Provide a sentence: ")
+
+if __name__ == "__main__":
+    summary_template = """
+        Shorten the following sentence: {information}
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    print(chain.run(information=information))
